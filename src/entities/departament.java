555df2793094c2869d970c1c5d17dc4f7a3d30be
Joao Valadares <joaogvaladares@outlook.com>
2022-12-09 21:23:45 -0300

    arquivos base do projeto

---
+++ src/entities/departament.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class departament {
+    
+    private String name;
+
+    public departament(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
